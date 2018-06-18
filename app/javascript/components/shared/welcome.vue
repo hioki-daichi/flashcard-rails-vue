c308829cec1e0046e065b8bd437b04010a6ad1ea
Daichi Hioki <hiokidaichi@gmail.com>
2018-06-19 03:36:36 +0900

    Redirect `/` to `/books`

--- app/javascript/components/shared/welcome.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Welcome to flashcard-rails-vue</h1>
-</template>
--- app/javascript/routes.js
+++ app/javascript/routes.js
@@ -1,12 +1,11 @@
 import VueRouter from "vue-router";
-import WelcomePage from "./components/shared/welcome.vue";
 import BookIndex from "./components/BookIndex.vue";
 import PageIndex from "./components/PageIndex.vue";
 
 const routes = [
   {
     path: "/",
-    component: WelcomePage
+    redirect: '/books'
   },
   {
     path: "/books",
