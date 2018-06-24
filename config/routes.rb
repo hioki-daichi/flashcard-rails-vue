245ebb23bcb03d882a54f891b54d916c77ef96a3
Daichi Hioki <hiokidaichi@gmail.com>
2018-06-24 23:18:10 +0900

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#welcome'
 
-  resources :books, only: [:index] do
-    resources :pages, only: [:index]
-  end
-
   namespace :api do
     resources :books, only: [:index, :create, :destroy] do
       resources :pages, only: [:index, :create, :destroy]
