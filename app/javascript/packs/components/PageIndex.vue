4bf553362b879646256e970528156cc2d1d6417d
Daichi Hioki <hiokidaichi@gmail.com>
2018-06-28 10:08:42 +0900

    Remove unnecessary html tags and classes

--- app/javascript/packs/components/PageIndex.vue
+++ app/javascript/packs/components/PageIndex.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <h2>Pages</h2>
-    <div class="flex-grid">
-      <div class="col">
-        <textarea placeholder="Question" v-model="question" />
-        <textarea placeholder="Answer" v-model="answer" />
-        <button @click="submit">Submit</button>
-      </div>
+    <div>
+      <textarea placeholder="Question" v-model="question" />
+      <textarea placeholder="Answer" v-model="answer" />
+      <button @click="submit">Submit</button>
     </div>
     <Page v-for="page in this.$store.state.pages" :page="page" :key="page.id"></Page>
   </div>
