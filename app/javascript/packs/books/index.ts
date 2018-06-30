57fb24cc34e94f03fb5b98a44620a7ae65638f0a
Daichi Hioki <hiokidaichi@gmail.com>
2018-06-30 09:17:51 +0900

    Remove unnecessary files

--- app/javascript/packs/books/index.ts
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import BookIndex from "../components/BookIndex.vue";
-
-document.addEventListener("DOMContentLoaded", () => {
-  new Vue(BookIndex).$mount("#app");
-});
--- app/javascript/packs/pages/index.ts
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import PageIndex from "../components/PageIndex.vue";
-
-document.addEventListener("DOMContentLoaded", () => {
-  new Vue(PageIndex).$mount("#app");
-});
