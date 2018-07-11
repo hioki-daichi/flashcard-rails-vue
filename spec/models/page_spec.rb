741b48afb6d3c85bb116308794d7b7b86695a751
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-11 23:07:11 +0900

    Add basic model specs

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it { is_expected.to belong_to(:book) }
+    it { is_expected.to validate_presence_of(:question) }
+    it { is_expected.to validate_presence_of(:answer) }
+  end
 end
