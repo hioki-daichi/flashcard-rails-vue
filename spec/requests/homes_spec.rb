2123093b2096578eda0eeba3f52e6eefc497dc7e
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-16 01:00:33 +0900

    Add homes request spec

---
+++ spec/requests/homes_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Api::HomesController, type: :request do
+  describe 'GET /' do
+    it 'returns http status 200' do
+      get '/'
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'GET /nonexistent_path' do
+    it 'returns http status 302 and redirects to root' do
+      get '/nonexistent_path'
+      expect(response).to have_http_status 302
+      expect(response.headers['Location']).to eq 'http://www.example.com/'
+    end
+  end
+end
