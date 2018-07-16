c4ce419fe9c1388c970b01ace3b5ab3ff593bf8f
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-16 13:35:28 +0900

    Bump webpacker, vue-loader, ts-loader, webpack-dev-server versions and use VueLoaderPlugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,11 @@
 const { environment } = require('@rails/webpacker')
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const typescript =  require('./loaders/typescript')
 const vue =  require('./loaders/vue')
 
+environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.append('vue', vue)
 environment.loaders.append('typescript', typescript)
+
 module.exports = environment
