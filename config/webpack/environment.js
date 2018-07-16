ca5476414b5132c8ceef3ceebbf86d6e6b8ffb38
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-16 13:44:24 +0900

    cosme

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const typescript =  require('./loaders/typescript')
-const vue =  require('./loaders/vue')
+const vue             = require('./loaders/vue')
+const typescript      = require('./loaders/typescript')
 
 environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
+
 environment.loaders.append('vue', vue)
 environment.loaders.append('typescript', typescript)
 
