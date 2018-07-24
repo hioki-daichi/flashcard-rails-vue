867eca8753a9201c2169642c7cdfb243abd4e068
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-25 07:47:32 +0900

    Add path to page factory

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :page do
     association :book
     sequence(:row_order) { |n| n }
+    path { Faker::Lorem.words.join('/') }
     question { Faker::Lorem.sentence }
     answer { Faker::Lorem.sentence }
   end
