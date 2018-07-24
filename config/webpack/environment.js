7b6239c9d199cf8376735a25420f16b26102366d
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-24 22:38:18 +0900

    Downgrade webpacker, vue-loader, ts-loader versions to enable to work on heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const vue             = require('./loaders/vue')
-const typescript      = require('./loaders/typescript')
-
-environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
+const vue        = require('./loaders/vue')
+const typescript = require('./loaders/typescript')
 
 environment.loaders.append('vue', vue)
 environment.loaders.append('typescript', typescript)
