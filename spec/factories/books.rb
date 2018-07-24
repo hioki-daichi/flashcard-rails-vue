00f7e144b7353fb41dade2cf69a120537437732c
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-25 07:49:50 +0900

    Fix book factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :book do
     association :user
     sequence(:row_order) { |n| n }
-    title { Faker::Book.title }
+    sequence(:title) { |n| "#{Faker::Book.title} #{n}" }
   end
 end
