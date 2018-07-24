f8ec77b5493e4cc8dc4266bd8a32ae9461fef079
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-25 07:16:31 +0900

    Comment out test_unit

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'action_view/railtie'
 # require 'action_mailer/railtie'
 require 'active_job/railtie'
 require 'action_cable/engine'
-require 'rails/test_unit/railtie'
+# require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
