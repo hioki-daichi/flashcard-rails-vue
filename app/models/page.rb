e7ec544d961fdf3515c7226c85d46ee428b47d82
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-26 22:40:50 +0900

    Change max length of question and answer from 1000 to 2048

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,6 +5,6 @@ class Page < ApplicationRecord
   belongs_to :book
 
   validates :path, length: { maximum: 255 }
-  validates :question, presence: true, length: { maximum: 1000 }
-  validates :answer, presence: true, length: { maximum: 1000 }
+  validates :question, presence: true, length: { maximum: 2048 }
+  validates :answer, presence: true, length: { maximum: 2048 }
 end
