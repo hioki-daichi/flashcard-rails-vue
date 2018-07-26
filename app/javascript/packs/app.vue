0c05b664455972aa3b28f9488bf82ee7d7c2c093
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-26 21:30:09 +0900

    Show alertMessage if exists

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -2,6 +2,7 @@
   <v-app :class="{ loading: this.$store.state.global.loading }">
     <Header />
     <v-content>
+      <v-alert :value="!!alertMessage" type="error">{{alertMessage}}</v-alert>
       <router-view />
     </v-content>
   </v-app>
@@ -13,6 +14,11 @@ import Header from "./components/shared/Header.vue";
 export default {
   components: {
     Header
+  },
+  computed: {
+    alertMessage() {
+      return this.$store.state.global.alertMessage;
+    }
   }
 };
 </script>
