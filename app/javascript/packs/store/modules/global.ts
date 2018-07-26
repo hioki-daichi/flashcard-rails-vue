e25119782466d777764dc5d8b8e138d4682fbf02
Daichi Hioki <hiokidaichi@gmail.com>
2018-07-26 21:18:30 +0900

    Add alertMessage to global module

--- app/javascript/packs/store/modules/global.ts
+++ app/javascript/packs/store/modules/global.ts
@@ -4,10 +4,12 @@ export default {
   namespaced: true,
   state: {
     previousUrl: null,
-    loading: false
+    loading: false,
+    alertMessage: null
   },
   mutations: {
     setPreviousUrl: mutator.set("previousUrl"),
-    setLoading: mutator.set("loading")
+    setLoading: mutator.set("loading"),
+    setAlertMessage: mutator.set("alertMessage")
   }
 };
