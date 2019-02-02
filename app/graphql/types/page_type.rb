0563375e15b7cbc296e110c8f9dd705d62cc54ac
Daichi Hioki <hiokidaichi@gmail.com>
2019-02-03 01:00:09 +0900

    Add type: PageType

---
+++ app/graphql/types/page_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class PageType < Types::BaseObject
+    field :sub,        ID,                              null: false
+    field :path,       String,                          null: false
+    field :question,   String,                          null: false
+    field :answer,     String,                          null: false
+    field :row_order,  Int,                             null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
