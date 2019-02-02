64dd0c191b32b68b3bc4ac34eeea4e6b774ae2d0
Daichi Hioki <hiokidaichi@gmail.com>
2019-02-03 01:00:34 +0900

    Add type: QueryType

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,23 @@
+module Types
+  class QueryType < Types::BaseObject
+    field :books, [BookType], null: false
+
+    field :book, BookType, null: true do
+      argument :sub, ID, required: true
+    end
+
+    def books
+      current_user.books.rank(:row_order)
+    end
+
+    def book(sub:)
+      current_user.books.rank(:row_order).find_by(sub: sub)
+    end
+
+    private
+
+    def current_user
+      context[:current_user]
+    end
+  end
+end
