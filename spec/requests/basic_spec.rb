45441fbb2bc8e33d5a8edac4f39ce411f4f2eade
Daichi Hioki <hiokidaichi@gmail.com>
2019-02-04 13:49:59 +0900

    Fix basic test

--- spec/requests/basic_spec.rb
+++ spec/requests/basic_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Basic specs', type: :request do
       let(:headers) { { Authorization: "Bearer: #{JWT.encode({ user_id: 1, exp: Time.now.to_i }, JsonWebToken.key, JsonWebToken::ALG)}" } }
 
       it 'returns http status 419 and token expired error' do
-        get '/api/books', headers: headers
+        post '/graphql', params: { 'query': '{ books { sub } }' }, headers: headers
         expect(response).to have_http_status 419
         expect(response.body).to eq "{\"errors\":[\"Token expired\"]}"
       end
@@ -16,7 +16,7 @@ RSpec.describe 'Basic specs', type: :request do
       let(:headers) { { Authorization: "Bearer: invalid.jwt" } }
 
       it 'returns http status 400 and token invalid error' do
-        get '/api/books', headers: headers
+        post '/graphql', params: { 'query': '{ books { sub } }' }, headers: headers
         expect(response).to have_http_status 400
         expect(response.body).to eq "{\"errors\":[\"Token invalid\"]}"
       end
